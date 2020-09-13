7da922f91daae2e882d2da001fbb7fffb71ca4e5
wuyaheng <wuyaheng2016@gmail.com>
2020-09-13 17:45:47 -0400

    hash password

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
-const express = require("express")
-
-const expressLayouts = require('express-ejs-layouts')
-
-const mongoose = require('mongoose')
-
+const express = require("express");
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
 const app = express();
-
+// DB Config
 const db = require('./config/keys').MongoURI;
-
-mongoose.connect(db, { useNewUrlParser: true})
-    .then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.log(err))
-
+// Connect to Mongo 
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('MongoDB connected!'))
+.catch(err => console.log(err));
 app.use(expressLayouts);
-app.set('view engine', 'ejs')
-
-
-app.use(express.urlencoded({ extended: false }))
-
-app.use('/', require('./routes/index'))
-app.use('/users', require('./routes/users'))
-
+app.set('view engine', 'ejs');
+// Bodyparser
+app.use(express.urlencoded({ extended: false}));
+// Express Session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  }));
+// Connect flash 
+app.use(flash());
+// Global vars 
+app.use((req,res,next)=> {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+})
 const PORT = process.env.PORT || 5000;
-
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
 app.listen(PORT, console.log(`Server started on ${PORT}`));
-
--- routes/users.js
+++ routes/users.js
@@ -1,37 +1,81 @@
 const express = require('express');
-
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
-router.get('/login', (req, res) => res.render('login'))
+// User model 
+const User = require('../models/User');
+// Login page
+router.get('/login', (req,res) => res.render('login'));
 
-router.get('/register', (req, res) => res.render('register'))
+// Register page
+router.get('/register', (req,res) => res.render('register'));
 
-router.post('/register', (req, res) => {
+// Register Handle
+router.post('/register', (req,res) => {
     const { name, email, password, password2 } = req.body;
     let errors = [];
-    if(!name || !email || !password || !password2) {
+
+    // Check required fields 
+    if(!name || !email || !password || !password2){
         errors.push({ msg: 'Please fill in all fields'});
     }
 
+    // Check passwords match 
     if(password !== password2) {
-        errors.push({ msg: 'Passwords do not match'})
+        errors.push({ msg: 'Passwords do not match'});
     }
 
+    // Check pass length 
     if(password.length < 6) {
-        errors.push({ msg: 'Password should be at least 6 characters'})
+        errors.push({ msg: 'Password should be at least 6 characters'});
     }
 
     if(errors.length > 0) {
-        res.render('register', {  
+        res.render('register', {
             errors,
             name,
             email,
             password,
-            password2 
+            password2
         });
     } else {
-        res.send('pass');
-    }
-})
+        // Validation passed 
+        User.findOne({ email: email })
+        .then(user => {
+            if(user) {
+                // User exists 
+                errors.push({ msg: 'Email is already registered'})
+                res.render('register', {
+                    errors,
+                    name,
+                    email,
+                    password,
+                    password2
+                });
+            } else {
+                const newUser = new User({
+                    name, 
+                    email,
+                    password
+                });
+                // Hash password
+                bcrypt.genSalt(10, (err, salt)=> 
+                    bcrypt.hash(newUser.password, salt, (err, hash)=>{
+                        if (err) throw err; 
+                        // set password to hashed 
+                        newUser.password = hash;
+                        // save user 
+                        newUser.save()
+                            .then(user =>{
+                                req.flash('success_msg', "you are now registered and can log in!");
+                                res.redirect('/users/login')
+                            })
+                            .catch( err=> console.log(err));
+                    }))
+                }
+            });
+        }
+});
+
 
 module.exports = router;
